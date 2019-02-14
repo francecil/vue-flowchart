91b35072bd7700b28f4b5147c28fc712e2382272
郑家兴 <zhengjx@wangsu.com>
2019-02-14 15:00:58 +0800

    删除无用文件，更新package.json

--- src/main.js
+++ src/main.js
@@ -3,13 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-import store from '@/store'
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
 Vue.config.productionTip = false
+Vue.use(ElementUI)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
-  store,
   components: { App },
   template: '<App/>'
 })
